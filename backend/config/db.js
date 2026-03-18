f3d7efe04179eb7eab3b327cb2095dea325a8c02
khxtrikk <khatrisakshi3003@gmail.com>
2026-03-18 22:07:24 +0530

    fix: remove dotenv dependency for k8s environment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,8 @@
-
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
+    mongoose.connection.on('connected', () => console.log("DB Connected"))
+    await mongoose.connect(process.env.MONGODB_URI)
+}
 
-export { connectDB };
+export default connectDB
